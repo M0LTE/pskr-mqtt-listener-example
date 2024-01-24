24d81fa65675a646b7b70c40946655580d8a0930
Tom Fanning <tom@m0lte.uk>
2024-01-24 21:52:35 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Extensions.ManagedClient;
+
+using var _mqttClient = new MqttFactory().CreateManagedMqttClient();
+
+var options = new ManagedMqttClientOptionsBuilder()
+    .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+    .WithClientOptions(new MqttClientOptionsBuilder()
+        .WithClientId(Guid.NewGuid().ToString())
+        .WithTcpServer("mqtt.pskreporter.info")
+        .Build())
+.Build();
+
+var topics = new[] {
+    // numbers are ARRL DXCC entity codes
+    new MqttTopicFilterBuilder().WithTopic("pskr/filter/v2/+/+/+/+/+/+/+/223").Build(),
+    new MqttTopicFilterBuilder().WithTopic("pskr/filter/v2/+/+/+/+/+/+/+/294").Build(),
+    new MqttTopicFilterBuilder().WithTopic("pskr/filter/v2/+/+/+/+/+/+/+/265").Build(),
+    new MqttTopicFilterBuilder().WithTopic("pskr/filter/v2/+/+/+/+/+/+/+/279").Build(),
+    new MqttTopicFilterBuilder().WithTopic("pskr/filter/v2/+/+/+/+/+/+/+/245").Build(),
+};
+
+await _mqttClient.SubscribeAsync(topics);
+await _mqttClient.StartAsync(options);
+
+_mqttClient.ApplicationMessageReceivedAsync += arg => {
+    Console.WriteLine(arg.ApplicationMessage.ConvertPayloadToString());
+    return Task.CompletedTask;
+};
+
+Console.WriteLine("Waiting for messages... press enter to quit");
+Console.ReadLine();
